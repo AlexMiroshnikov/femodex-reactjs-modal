94f2c42bac35e7020d1543d2b7a38b2aca193012
AlexMiroshnikov <ilex@ilex-thinknote>
2015-09-20 15:29:34 +0700

    Move modal logic into parent Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,6 +1,18 @@
 var Femodex = Femodex || {};
 Femodex.Components = Femodex.Components || {};
 Femodex.Components.Modal = React.createClass({
+    componentDidMount: function(){
+        this._getOverlay().style.display = 'block';
+    },
+    _getOverlay: function(){
+        return React.findDOMNode(this).parentNode;
+    },
+    cancel: function(){
+        var overlay = this._getOverlay();
+        overlay.style.display = 'none';
+        React.unmountComponentAtNode(overlay);
+        this.props.modal.onCancel();
+    },
     render: function(){
         var content = (
             this.props.modal ?
@@ -11,6 +23,10 @@ Femodex.Components.Modal = React.createClass({
             <div className="fmx-modal-container">
                 <a href="#" onClick={this.cancel} className="fmx-link-cancel">&times;</a>
                 {content}
+                <div className="fmx-modal-control">
+                    <button className="fmx-btn-ok">{this.props.modal.getTextOk()}</button>
+                    <button className="fmx-btn-cancel" onClick={this.cancel}>{this.props.modal.getTextCancel()}</button>
+                </div>
             </div>
         );
     }
--- src/components/ModalContent.jsx
+++ src/components/ModalContent.jsx
@@ -1,25 +1,7 @@
 var Femodex = Femodex || {};
 Femodex.Components = Femodex.Components || {};
 Femodex.Components.ModalContent = React.createClass({
-    componentDidMount: function(){
-        this._getOverlay().style.display = 'block';
-    },
-    _getOverlay: function(){
-        return React.findDOMNode(this.props.parent).parentNode;
-    },
-    cancel: function(){
-        var overlay = this._getOverlay();
-        overlay.style.display = 'none';
-        React.unmountComponentAtNode(overlay);
-        this.props.modal.onCancel();
-    },
     render: function(){
-        return (
-            <div>
-                <p>{this.props.modal.getMessage()}</p>
-                <button className="fmx-btn-ok">{this.props.modal.getTextOk()}</button>
-                <button onClick={this.cancel} className="fmx-btn-cancel">{this.props.modal.getTextCancel()}</button>
-            </div>
-        );
+        return (<div className="fmx-modal-content">{this.props.modal.getMessage()}</div>);
     }
 });
